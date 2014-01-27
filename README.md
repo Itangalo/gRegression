a2df0ca65c499f47778bef32d43e68ca8fe74ccd
Itangalo <johan@vaxjonexus.com>
2014-01-27 08:16:34 +0100

    Version 1.1. Function expression is now displayed directly in the gRegression dialog.

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Google Drive script for doing regression analysis of spreadsheet data. Builds on
 
 How to use
 ==========
-Select x and y data by click-and-drag in the spreadsheet, then run "regression analysis" from the menu and select the relevant options. (Data must be in columns, with x values in the first selected column.)
-Use the option "Create a new sheet with regression-fitted data and a scatter plot" to have the function-fitted data plotted in a chart with no extra work.
+Select x and y data by click-and-drag in the spreadsheet, then run "regression analysis" from the menu and select the relevant function type. (Data must be in columns, with x values in the first selected column.)
+Use the button "Build scatter plot" to have the regression-fitted data plotted in a chart, in a new sheet.
 
 
 How to install
@@ -18,4 +18,4 @@ You have three different options for installing gRegression:
 
 2: Use this link to copy a spreadsheet already containing the code: https://docs.google.com/spreadsheet/ccc?key=0AjgECFpHWbvRdGFXeFNrbWUyV05FVTE2bHZFTXM4WWc&newcopy
 
-3: Use the source code found here on GitHub and paste it into the code editor in a Google spreadsheet. If you do, you will also need to add the code from regression-js manually (see link to Tom Alexander's project above). You might want this patch, too: https://github.com/Tom-Alexander/regression-js/pull/7
+3: Use the source code found here on GitHub and paste it into the code editor in a Google spreadsheet. If you do, you will also need to add the code from regression-js manually (see link to Tom Alexander's project above).
--- gRegression.js
+++ gRegression.js
@@ -19,8 +19,9 @@ function onInstall() {
 
 // The dialog for the regression analysis settings.
 function regressionDialog() {
-  var app = UiApp.createApplication().setTitle('Google spreadsheet regression analysis 1.0');
+  var app = UiApp.createApplication().setTitle('Google spreadsheet regression analysis 1.1');
   var handler = app.createServerHandler('regressionHandler');
+  var equationHandler = app.createServerHandler('displayEquation');
 
   // Build a list of regression modes.
   var modes = {
@@ -31,44 +32,49 @@ function regressionDialog() {
     polynomial : 'Polynomial function a0x^0 ... + anx^n',
   };
   app.add(app.createLabel('Select type of function'));
-  var modeList = app.createListBox().setName('mode')
+  var modeList = app.createListBox().setName('mode').addChangeHandler(equationHandler);
   for (var mode in modes) {
     modeList.addItem(modes[mode], mode);
   }
   app.add(modeList);
   handler.addCallbackElement(modeList);
+  equationHandler.addCallbackElement(modeList);
 
   // Add some extras used only for polynomial functions.
   var visibilitySwitcher = app.createServerHandler('visibilitySwitcher').addCallbackElement(modeList);
   modeList.addChangeHandler(visibilitySwitcher);
   app.add(app.createLabel('…of order').setId('polynomialLabel').setVisible(false));
-  var order = app.createListBox().setName('polynomialOrder').setId('polynomialOrder').setVisible(false).addItem('2', 2).addItem('3', 3).addItem('4', 4).addItem('5', 5);
+  var order = app.createListBox().setName('polynomialOrder').setId('polynomialOrder').addChangeHandler(equationHandler).setVisible(false).addItem('2', 2).addItem('3', 3).addItem('4', 4).addItem('5', 5);
   handler.addCallbackElement(order);
+  equationHandler.addCallbackElement(order);
   app.add(order);
 
-  // Some extra options.
-  app.add(app.createHTML('<br />'));
-  var addSheet = app.createCheckBox('Create a new sheet with regression-fitted data and a scatter plot') .setName('addSheet');
-  app.add(addSheet);
-  handler.addCallbackElement(addSheet);
+  var equationContainer = app.createCaptionPanel('Best fit').setId('equation').setWidth('90%').setStyleAttribute('border', 'thin black solid').setStyleAttribute('padding', '4px');
+  app.add(equationContainer);
 
   // Add buttons for ok an cancel.
-  app.add(app.createHTML('<hr />'));
   var close = app.createServerHandler('close');
   var cancel = app.createButton('Cancel').setId('cancel').addClickHandler(close);
   app.add(cancel);
-  var ok = app.createButton('Run').setId('ok').addClickHandler(handler);
+  var ok = app.createButton('Build scatter plot').setId('ok').addClickHandler(handler);
   app.add(ok);
 
+  var eventInfo = {
+    parameter : {
+      mode : 'linear',
+    },
+  };
+  displayEquation(eventInfo);
+
   // Run some validations.
-  var errorMessage = app.createLabel().setId('errorMessage');
-  app.add(errorMessage);
   if (SpreadsheetApp.getActiveRange().getNumColumns() != 2) {
-    errorMessage.setText('Error: You must select two columns, containing x and y values.');
+    equationContainer.clear();
+    equationContainer.add(app.createLabel('Error: You must select two columns, containing x and y values.'));
     ok.setEnabled(false);
   }
   if (SpreadsheetApp.getActiveRange().getNumRows() < 2) {
-    errorMessage.setText('Error: You must select at least two rows, containing coordinates for points.');
+    equationContainer.clear();
+    equationContainer.add(app.createLabel('Error: You must select at least two rows, containing coordinates for points.'));
     ok.setEnabled(false);
   }
 
@@ -94,47 +100,65 @@ function visibilitySwitcher(eventInfo) {
   return app;
 }
 
-// The function coordinating the regression analysis.
-function regressionHandler(eventInfo) {
+// Displays the best-fit equation in the panel.
+function displayEquation(eventInfo) {
+  var app = UiApp.getActiveApplication();
+  var container = app.getElementById('equation');
+  container.clear();
+
   if (eventInfo.parameter.mode == 'polynomial' && SpreadsheetApp.getActiveRange().getNumRows() <= parseInt(eventInfo.parameter.polynomialOrder)) {
-    Browser.msgBox('Error', 'You need more data points to do polynomial regression of this order. Select more rows and try again.', null);
-    return UiApp.getActiveApplication().close();
+    container.add(app.createLabel('You need more data points to do polynomial regression of this order. Select more rows and try again.'));
+    app.getElementById('ok').setEnabled(false);
+    return app;
   }
 
   // Get the best fit, and display it.
   var values = SpreadsheetApp.getActiveRange().getValues();
   var result = window.regression(eventInfo.parameter.mode, values, parseInt(eventInfo.parameter.polynomialOrder));
-  Browser.msgBox('Best fit', result.string, null);
-
-  // Build a new sheet, if asked to.
-  if (eventInfo.parameter.addSheet == 'true') {
-    var newSheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet();
-    newSheet.activate();
-    // Setting some titles.
-    newSheet.getRange('A1').setValue('Function:');
-    newSheet.getRange('B1').setValue(result.string);
-    newSheet.getRange('A2').setValue('x');
-    newSheet.getRange('B2').setValue('y (original)');
-    newSheet.getRange('C2').setValue('y (fitted)');
-
-    // Fill in the values from the original selection, and the regression.
-    newSheet.getRange(3, 1, values.length, 2).setValues(values);
-    var row = 3;
-    for (var i in result.points) {
-      newSheet.getRange(row, 3).setValue(result.points[i][1]);
-      row++;
-    }
-
-    // Build a scatter chart.
-    var dataRange = newSheet.getRange(2, 1, values.length, 3);
-    var chart = newSheet.newChart()
-      .setChartType(Charts.ChartType.SCATTER)
-      .addRange(dataRange)
-      .setPosition(1, 4, 0, 0)
-      .setOption('title', result.string)
-      .build();
-    newSheet.insertChart(chart);
+  if (result.equation[0].toString() == 'NaN') {
+    app.getElementById('ok').setEnabled(false);
+    container.add(app.createLabel('Cannot parse data. Does it contain non-numbers, or values outside the domain for the function?'));
+    return app;
   }
+
+  app.getElementById('ok').setEnabled(true);
+  container.add(app.createLabel(result.string));
+  return app;
+}
+
+// Builds a new sheet with the fitted and original data, and builds a scatter plot from it.
+function regressionHandler(eventInfo) {
+  // Get the best fit.
+  var values = SpreadsheetApp.getActiveRange().getValues();
+  var result = window.regression(eventInfo.parameter.mode, values, parseInt(eventInfo.parameter.polynomialOrder));
+
+  // Build a new sheet.
+  var newSheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet();
+  newSheet.activate();
+  // Setting some titles.
+  newSheet.getRange('A1').setValue('Function:');
+  newSheet.getRange('B1').setValue(result.string);
+  newSheet.getRange('A2').setValue('x');
+  newSheet.getRange('B2').setValue('y (original)');
+  newSheet.getRange('C2').setValue('y (fitted)');
+
+  // Fill in the values from the original selection, and the regression.
+  newSheet.getRange(3, 1, values.length, 2).setValues(values);
+  var row = 3;
+  for (var i in result.points) {
+    newSheet.getRange(row, 3).setValue(result.points[i][1]);
+    row++;
+  }
+
+  // Build a scatter chart.
+  var dataRange = newSheet.getRange(2, 1, values.length, 3);
+  var chart = newSheet.newChart()
+  .setChartType(Charts.ChartType.SCATTER)
+  .addRange(dataRange)
+  .setPosition(1, 4, 0, 0)
+  .setOption('title', result.string)
+  .build();
+  newSheet.insertChart(chart);
 }
 
 // Simple handler for closing the active UI.
@@ -144,10 +168,10 @@ function close(eventInfo) {
 
 // Displays help information.
 function help() {
-  var app = UiApp.createApplication().setTitle('Google spreadsheet regression analysis 1.0');
+  var app = UiApp.createApplication().setTitle('Google spreadsheet regression analysis 1.1');
   var handler = app.createServerHandler('close');
-  app.add(app.createHTML('<strong>How to use:</strong> Select x and y data by click-and-drag in the spreadsheet, then run "regression analysis" from the menu and select the relevant options. (Data must be in columns, with x values in the first selected column.)<br/>'));
-  app.add(app.createHTML('Use the option "Create a new sheet with regression-fitted data and a scatter plot" to have the function-fitted data plotted in a chart with no extra work.<br/><br/>'));
+  app.add(app.createHTML('<strong>How to use:</strong> Select x and y data by click-and-drag in the spreadsheet, then run "regression analysis" from the menu and select the relevant function type. (Data must be in columns, with x values in the first selected column.)<br/>'));
+  app.add(app.createHTML('Use the button "Build scatter plot" to have the regression-fitted data plotted in a chart, in a new sheet.<br/><br/>'));
 
   app.add(app.createHTML('Thanks to Thanks to Tom Alexander for the JavaScript library for regression analysis: https://github.com/Tom-Alexander/regression-js<br/><br/>'));
   app.add(app.createHTML('Follow the link below to find more information about this project, including source code and issue queue.<br/><br/>'));
