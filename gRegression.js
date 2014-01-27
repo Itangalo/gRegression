70dd9777b764b590eb39939cbb92b933d1a75c9a
Itangalo <johan@vaxjonexus.com>
2014-01-27 08:23:59 +0100

    Dialog now closes after the chart has been built.

--- gRegression.js
+++ gRegression.js
@@ -159,6 +159,8 @@ function regressionHandler(eventInfo) {
   .setOption('title', result.string)
   .build();
   newSheet.insertChart(chart);
+
+  return UiApp.getActiveApplication().close();
 }
 
 // Simple handler for closing the active UI.
